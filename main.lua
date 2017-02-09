015782a06b74ca6c1e446ab0047c086c2fdd831a
rbz55 <ryan@barzion.net>
2017-02-08 16:41:13 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,17 @@
------------------------------------------------------------------------------------------
+
+--main.lua
+
+
+
+display.setStatusBar(display.HiddenStatusBar)
+
+
+
+local composer = require("composer")
+
+
+
+composer.gotoScene("start")--------------------------------------------------------------------------------------
 --
 -- main.lua
 --
